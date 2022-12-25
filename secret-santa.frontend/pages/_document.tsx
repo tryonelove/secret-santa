d3e9f91eeeafb35b9830f95afd08a3192acd4f7a
Ilya <tryonelove@proton.me>
2022-12-25 05:07:45 +0300

    Dark Theme - Add dark theme support

--- secret-santa.frontend/pages/_document.tsx
+++ secret-santa.frontend/pages/_document.tsx
@@ -1,12 +1,15 @@
 import { ColorModeScript } from '@chakra-ui/react';
 import { Html, Head, Main, NextScript } from 'next/document';
+import { theme } from 'assets/theme';
 
 export default function Document() {
     return (
         <Html lang='en'>
             <Head />
             <body>
-                <ColorModeScript initialColorMode='dark' />
+                <ColorModeScript
+                    initialColorMode={theme.config.initialColorMode}
+                />
                 <Main />
                 <NextScript />
             </body>
